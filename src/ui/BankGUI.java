d10c8180c6eb6ef02bb902a7fd0177f875f61582
kevinAndresF <54720000+kevinAndresF@users.noreply.github.com>
2020-09-30 14:50:43 -0500

    add fxml

--- src/ui/BankGUI.java
+++ src/ui/BankGUI.java
@@ -8,8 +8,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
@@ -124,5 +128,106 @@ public class BankGUI {
     void withDraw(ActionEvent event) {
 
     }
-	
+    
+    ///////////////////////////////////////////////////// Queue General & priority /////////////////////////////////////////////////
+    
+    @FXML
+    private TableView<?> tableGeneral;
+
+    @FXML
+    private TableColumn<?, ?> ColumnGeneral;
+
+    @FXML
+    private TableView<?> tablePriority;
+
+    @FXML
+    private TableColumn<?, ?> columnPriority;
+
+    @FXML
+    private TextField txtCedulaCiudadania;
+
+    @FXML
+    private TextField txtNameAdd;
+
+    @FXML
+    private Button btAddGeneralRow;
+
+    @FXML
+    private Button btAddPriorityRow;
+
+    @FXML
+    void addGeneralRow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addPriorityRow(ActionEvent event) {
+
+    }
+    
+    ///////////////////////////////////////////////// Because Priority /////////////////////////////////////////////////
+    
+    @FXML
+    private Button btReady;
+
+    @FXML
+    private RadioButton rbDisabled;
+
+    @FXML
+    private ToggleGroup grupPriority;
+
+    @FXML
+    private RadioButton rbPregnancy;
+
+    @FXML
+    private RadioButton rbSeniorAdult;
+
+    @FXML
+    private RadioButton rbYounger;
+
+    @FXML
+    void ready(ActionEvent event) {
+
+    }
+    
+    ///////////////////////////////////////////////// clients sort /////////////////////////////////////////////////
+    
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnName;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnCC;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnTime;
+
+    @FXML
+    private TableColumn<?, ?> tableColumnMoney;
+
+    @FXML
+    private RadioButton rbClientName;
+
+    @FXML
+    private ToggleGroup sortBy;
+
+    @FXML
+    private RadioButton rbCC;
+
+    @FXML
+    private RadioButton rbTime;
+
+    @FXML
+    private RadioButton rbMoney;
+
+    @FXML
+    private Button buttomReady;
+
+    @FXML
+    void readySort(ActionEvent event) {
+
+    }
+    
 }
